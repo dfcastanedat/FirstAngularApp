da7b7d24ca54d3dbf617b454d1442fefffcb3e8d
Daniel Castañeda <you@example.com>
2020-07-21 00:51:53 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component'
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component'
+//Rutas
+import {APP_ROUTING} from './app.routes'
 
+//Servicios
+
+
+//Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
-  bootstrap: [AppComponent, HeaderComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
